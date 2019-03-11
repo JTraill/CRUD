06488c6b7c6641cb71ef90e766231d23279f75f2
jtraill <joshdtraill@gmail.com>
2019-03-10 18:55:39 -0700

    Routed home page to root and displays products

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {RouterModule} from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpModule } from '@angular/http';
+import { Component, OnInit } from '@angular/core';
+import {Http, Response, Headers} from '@angular/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    RouterModule.forRoot([
+      {path: "", component: HomeComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
